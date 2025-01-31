86e66cdc745f27410aaa0def59f61ea63f53fd0f
Luis Parente <lgsp25@hotmail.com>
2025-01-31 10:41:03 -0300

    Criado classe Participante e seed do banco de dados

--- src/main/java/com/devsuperior/desafio2/entities/Atividade.java
+++ src/main/java/com/devsuperior/desafio2/entities/Atividade.java
@@ -1,14 +1,16 @@
 package com.devsuperior.desafio2.entities;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
@@ -29,7 +31,11 @@ public class Atividade {
 	private Categoria categoria;
 
 	@OneToMany(mappedBy = "atividade")
-	private List<Bloco> blocos = new ArrayList<>();
+	private Set<Bloco> blocos = new HashSet<>();
+
+	@ManyToMany
+	@JoinTable(name = "tb_atividade_participante", joinColumns = @JoinColumn(name = "atividade_id"), inverseJoinColumns = @JoinColumn(name = "participante_id"))
+	private Set<Participante> participantes = new HashSet<>();
 
 	public Atividade() {
 
@@ -83,8 +89,12 @@ public class Atividade {
 		this.categoria = categoria;
 	}
 
-	public List<Bloco> getBlocos() {
+	public Set<Bloco> getBlocos() {
 		return blocos;
+	}
+
+	public Set<Participante> getParticipantes() {
+		return participantes;
 	}
 
 	@Override
